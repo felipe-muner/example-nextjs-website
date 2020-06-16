2c01de80f4393c71f794527d24c4d0403bf57c22
felipemuner <felipe.muner@gmail.com>
2020-06-16 17:32:42 +0700

    add .env.local file to storage secret keys, as well client side

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../style.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  console.log(pageProps);
-  console.log(Component);
+  // console.log(pageProps);
+  // console.log(Component);
   return <Component {...pageProps} />;
 }
---
+++ pages/mysql-example.js
@@ -0,0 +1,22 @@
+export async function getStaticProps() {
+  console.log("mysql-example");
+  return {
+    props: {
+      items: [1, 2, 3, 4],
+    },
+  };
+}
+
+function Index({ items }) {
+  console.log(items);
+  console.log(process.env);
+  console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+  return (
+    <div>
+      <h1>image page1213</h1>
+      <div></div>
+    </div>
+  );
+}
+
+export default Index;
